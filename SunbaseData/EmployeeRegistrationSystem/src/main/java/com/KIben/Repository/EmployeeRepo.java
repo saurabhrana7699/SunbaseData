50aecaccb210d7a67825876837de83b7992bfbda
Saurabh Rana <106344770+saurabhrana7699@users.noreply.github.com>
2023-08-01 22:58:08 +0530

    d

--- SunbaseData/EmployeeRegistrationSystem/src/main/java/com/KIben/Repository/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.KIben.Repository;
-
-import com.KIben.Bean.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepo extends JpaRepository<Employee,Integer> {
-}
